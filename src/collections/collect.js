a685f9e08e19793785e938800e04ac379c7c0137
伯方 <bofang.zxj@taobao.com>
2014-07-28 14:19:10 +0800

    update _.collect

---
+++ src/collections/collect.js
@@ -0,0 +1,10 @@
+var _ = {};
+_.collect = function(arr,callback){
+    var result = [];
+    var len = arr.length;
+    for(var i= 0;i<len;i++){
+        result.push(callback(arr[i]))
+
+    }
+    return result;
+}
