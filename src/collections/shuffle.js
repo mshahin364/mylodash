2eba6bf976d1e3cd6177a6e50578179fa67d98b0
wechatgame <wechatgame@126.com>
2014-08-12 11:43:47 +0800

    shuffle

--- src/collections/shuffle.js
+++ src/collections/shuffle.js
@@ -1,6 +1,12 @@
 _.shuffle = function(collections){
     var ret = [];
+    var len = collections.length;
+    var index = -1;
     for(var i in collections){
+        index++;
 
+        var key = Math.floor(Math.random() * index);
+        ret[key] = collections[i];
+        ret[index] = collections[i];
     }
 }
