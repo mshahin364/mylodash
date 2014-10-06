e19a6a36911a3471474f9e39be03f6107a7e8fbc
伯方 <bofang.zxj@taobao.com>
2014-10-06 20:53:43 +0800

    grunt concat & uglify

--- GruntFile.js
+++ GruntFile.js
@@ -5,6 +5,18 @@ grunt.initConfig({
             src: ['src/*/**.js'],
             dest: 'lodash.js'
         }
+    },
+    uglify: {
+        options: {
+            banner: '/*! <%= grunt.template.today("yyyy-mm-dd") %> */\n;(function(){',//添加banner,
+            footer:'})();\n/*!  最后修改于： <%= grunt.template.today("yyyy-mm-dd") %> */'//添加footer
+        },
+        uglodash:{
+            files: {
+                './lodash-min.js': ['./lodash.js']
+            }
+        }
+
     }
 });
 grunt.loadNpmTasks('grunt-contrib-concat');
@@ -12,4 +24,4 @@ grunt.loadNpmTasks('grunt-contrib-uglify');
 
 
 
-grunt.registerTask('default', [ 'concat' ]);
+grunt.registerTask('default', [ 'concat','uglify:uglodash' ]);
